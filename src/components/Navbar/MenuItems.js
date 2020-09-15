185b93ab8bb1212216588dc35c8daf257355c5bf
gabsjorge <gabs.luluzinha@gmail.com>
2020-09-14 22:09:09 -0300

    Creating navbar and styling

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Compiler',
+        url: '/compiler',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Virtual Machine',
+        url: '/virtual-machine',
+        cName: 'nav-links'
+    },
+];
