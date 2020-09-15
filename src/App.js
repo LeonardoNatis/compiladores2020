ddbf90da7f2ec19af882d5c6573588b8e763b9e8
gabsjorge <gabs.luluzinha@gmail.com>
2020-09-14 22:21:55 -0300

    Refactor folder with components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './React components/Navbar/Navbar';
 import "./App.css";
 
 import Routes from './routes';
