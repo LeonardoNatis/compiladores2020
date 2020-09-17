7485d987fd96b3a8ddaee4652ea1366db47ed55d
gabsjorge <gabs.luluzinha@gmail.com>
2020-09-17 19:05:11 -0300

    Creating compiler page

--- src/pages/Compiler/index.js
+++ src/pages/Compiler/index.js
@@ -5,21 +5,80 @@ import '../../React components/Button.css';
 
 import Upload from '../../services/upload';
 
+import { analyseLexic, printTokenList } from '../../components/lexicAnalyzer';
+
 export default function Compiler() {
-    const [instrucao, setInstrucao] = useState([]);
+    const [code, setCode] = useState([]);
 
     const onChangeFile = (file) => {
-        setInstrucao(
-        file.split("\r\n").map((instrucao) => ({ instrucao, breakpoint: false }))
-        );
+        setCode( file.toString() );
+        // setCode( file.filter(line => line !== '' && line !== null).toString() );
+        // setCode( file.split("\n\t").toString() );
+        // setCode( file.split("\n\t").filter(line => line !== '' && line !== null).toString() );
     };
 
+    // const list = analyseLexic(code);
+
+    const runLexicAnalyser = () => {
+        
+        console.log(code);
+        console.log(typeof code);
+        const lexicTokens = analyseLexic(code);
+        console.log(lexicTokens);
+        printTokenList(lexicTokens);
+        
+    }
+    
     return (
         <>
             <h1>Compiler</h1>
+            <br />
             <Button>
                 <Upload onChangefile={onChangeFile} />
             </Button>
+            <br />
+            <br />
+            <Button type="button" onClick={runLexicAnalyser}>Analise Lexic</Button>
         </>
     );
+
+    // const fileInput = '';
+
+    // function onSubmitFile() {
+    //     const item = document.getElementById('fileEntry');
+    //     if (item) {
+    //         item.click();
+    //     }
+    // }
+
+    // function onFileChange(event) {
+    //     if(event.target.files.length) {
+    //         fileInput = event.target.files[0];
+    //         readFile();
+    //     }
+    // }
+
+    // function readFile() {
+    //     const fileReader = new FileReader();
+    //     fileReader.onload = e => {
+    //         parseCode(fileReader.result);
+    //     };
+    //     fileReader.readAsText(fileInput);
+    // }
+
+    // function parseCode(){
+
+    // }
+
+    // return (
+    //     <>
+    //         <h1>Compiler</h1>
+    //         <br />
+    //         <Button>
+    //             <input id="fileEntry" type="file" accept=".txt" onChange={onFileChange(event)}/>
+    //         </Button>
+    //         <br />
+    //         <button type="button" onClick={onSubmitFile()}></button>
+    //     </>
+    // );
 }
