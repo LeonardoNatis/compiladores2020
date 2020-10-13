1020f427b1010707b35065bb75bcc93e4e90da3d
gabsjorge <gabs.luluzinha@gmail.com>
2020-10-13 00:25:58 -0300

    Adding syntactic errors

--- src/components/Errors.js
+++ src/components/Errors.js
@@ -1,3 +1,30 @@
+// Constante que possui os erros sintáticos com qual erro e mensagem de erro correspondente
+const syntacticErrors = {
+    EXPECTING_DOT: 'Ponto (.) não encontrado',
+    EXPECTING_SEMICOLON: 'Faltando ponto e vírgula (;)',
+    EXPECTING_OPEN_PARENTHESIS: 'Falta o abre parênteses',
+    EXPECTING_CLOSE_PARENTHESIS: 'Falta o fecha parênteses',
+    EXPECTING_FAÇA_TOKEN: `Token 'faça' não encontrado`,
+    EXPECTING_ENTAO_TOKEN: `Token 'entao' não encontrado`,
+    EXPECTING_COLON: 'Faltando dois pontos (:)',
+    MISSING_PROGRAM_NAME: 'Faltando o nome do programa',
+    MISSING_PROGRAM_TOKEN: 'Token de programa não encontrado',
+    INVALID_VARIABLE_NAME: 'Nome de variável inválida',
+    INVALID_VARIABLE_DECLARATION: 'Declaração de variável inválida',
+    INVALID_DATA_TYPE: 'Tipo de dado inválido',
+    INVALID_READ_COMMAND: `Comando 'leia' inválido`,
+    INVALID_WRITE_COMMAND: `Comando 'escreva' inválido`,
+    INVALID_PROCEDURE_DECLARATION: 'Declaração de procedimento inválida',
+    INVALID_FUNCTION_DECLARATION: 'Declaração de função inválida',
+    INVALID_EXPRESSION: 'Expressão inválida',
+    DUPLICATED_VARIABLE: 'Variável duplicada',
+    DUPLICATED_PROCEDURE: 'Procedimento duplicado',
+    DUPLICATED_FUNCTION: 'Função duplicada',
+    NOT_A_STATEMENT: 'Comando inválido',
+    UNEXPECTED_CHARACTER: 'Caractere não esperado',
+    IDENTIFIER_NOT_DECLARED: 'Identificador não declarado',
+}
+
 // Constante que possui os erros léxicos com qual erro e mensagem de erro correspondente
 const lexicErrors = {
     INVALID_CHARACTER: 'Caracter inválido',
@@ -38,3 +65,4 @@ class fileError extends Error {
 }
 
 export { fileErrors, lexicErrors };
+export { syntacticErrors };
